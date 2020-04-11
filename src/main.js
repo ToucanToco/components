202a6d4683f826407ac216a6f8eec110260010bf
David Nowinsky <david@nowinsky.net>
2020-04-11 11:57:36 +0200

    Export all components

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@
  *
  * All elements should be exported here.
  */
+export { TcAppBar } from './components/TcAppBar';
 export { TcButton } from './components/TcButton';
+export { TcCheckbox } from './components/TcCheckbox';
+export { TcClickable } from './components/TcClickable';
+export { TcDialog } from './components/TcDialog';
+export { TcForm } from './components/TcForm';
+export { TcFormField } from './components/TcFormField';
+export { TcIcon } from './components/TcIcon';
+export { TcNavigationDrawer } from './components/TcNavigationDrawer';
+export { TcRadioGroup } from './components/TcRadioGroup';
+export { TcSelect } from './components/TcSelect';
+export { TcSwitch } from './components/TcSwitch';
+export { TcTabs } from './components/TcTabs';
+export { TcText } from './components/TcText';
+export { TcTextField } from './components/TcTextField';
+export { TcToolbar } from './components/TcToolbar';
