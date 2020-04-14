584cdb79ab2a962941cd486bbdc6bda1f155b936
David Nowinsky <david@nowinsky.net>
2020-04-14 20:00:25 +0200

    Export all components, default export is the plugin

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from 'toucan-connectors/docs';
+import HelloWorld from '';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
